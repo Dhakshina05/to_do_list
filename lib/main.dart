f874c781e94c3388e501353f9c5fb9a0ad3aba03
unknown <sdhakshinashri05@gmail.com>
2025-09-04 20:12:32 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'pages/home_page.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'pages/login_page.dart';
+
+void main() async {
+  // init  the hive
+  await Hive.initFlutter();
+
+  //open a box
+  var box = await Hive.openBox('mybox');
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: Colors.pinkAccent),
+      initialRoute: "/login",
+      routes: {
+        "/login": (context) => LoginPage(),
+        "/": (context) => const HomePage(),
+      },
+    );
+  }
+}
